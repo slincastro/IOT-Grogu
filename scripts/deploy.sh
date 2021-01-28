9f2ca956fd44c6aa604a413f515432dfbf389944
Slin Castro <scastro@thoughtworks.com>
2021-01-27 23:56:09 -0500

    [#1] fix deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,11 +8,11 @@ echo "----------------------------------------------"
 echo "----------------------------------------------"
 echo "Deploying Artifacts"
 echo "----------------------------------------------"
-ampy --port /dev/tty.SLAB_USBtoUART put ./domain/
-ampy --port /dev/tty.SLAB_USBtoUART put ./configuration/
-ampy --port /dev/tty.SLAB_USBtoUART put ./util/
-ampy --port /dev/tty.SLAB_USBtoUART put grogu.py main.py
-ampy --port /dev/tty.SLAB_USBtoUART put config.json
+ampy --port /dev/tty.SLAB_USBtoUART put ./src/domain/
+ampy --port /dev/tty.SLAB_USBtoUART put ./src/configuration/
+ampy --port /dev/tty.SLAB_USBtoUART put ./src/util/
+ampy --port /dev/tty.SLAB_USBtoUART put ./srcg/rogu.py ./src/main.py
+ampy --port /dev/tty.SLAB_USBtoUART put ./src/config.json
 echo "---------------Folder------------------"
 ampy --port /dev/tty.SLAB_USBtoUART ls 
 echo "---------------Domain Folder------------------"
